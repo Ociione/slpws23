4a5c1df749b0beae08a5d646f4148e66aea85c06
Ociione <57672477+Ociione@users.noreply.github.com>
2023-03-29 14:25:53 +0200

    Funktion att se sina item och att öppna items

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'jwt'
+require_relative 'model.rb'
 
 enable :sessions
 
@@ -22,15 +23,20 @@ get('/') do
         redirect('/login')
     end
 
-    # db = SQLite3::Database.new('db/database.db')
-    # db.results_as_hash = true
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
 
-    # cases = db.execute("SELECT * FROM case")
-    # balance = db.execute("SELECT balance FROM user WHERE id = ?")
+    skrin = db.execute("SELECT * FROM cases")
+    balance = db.execute("SELECT balance FROM user WHERE id = ?", decoded_token[0]['sub'])
+    email = db.execute("SELECT email FROM user WHERE id = ?", decoded_token[0]['sub'])
+    items = db.execute("SELECT * FROM u_item WHERE user_id = ?", decoded_token[0]['sub'])
+    name = db.execute("SELECT item.name FROM u_item LEFT JOIN item ON u_item.item_id = item.id")
+    
+    db.close
 
-    # db.close
+    j = 0
 
-    slim(:index)
+    slim(:index, locals:{skrin:skrin, balance:balance, email:email, items:items, name:name, j:j})
 end
 
 get('/register') do
@@ -64,7 +70,7 @@ post('/register') do
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
     password = BCrypt::Password.create(params['password'])
-    starting_balance = 5.0
+    starting_balance = 10.0
 
     begin
         db.execute("INSERT INTO user (email, password, balance) VALUES (?, ?, ?)", email, password, starting_balance)
@@ -105,4 +111,42 @@ post('/login') do
     end
 
     redirect('/login?error=1')
+end
+
+post("/opencase/:id") do
+
+    token = session[:token]
+    verified, decoded_token = verify_token(token)
+    if !verified
+        redirect('/login')
+    end
+
+    caseid = params[:id]
+
+    #odds at 50:20:15:10:5
+    condition_float = rand()
+    caseRoll = rand()
+    if caseRoll <= 0.05 
+        rarity = 5
+    elsif caseRoll <= 0.15 
+        rarity = 4
+    elsif caseRoll <= 0.3 
+        rarity = 3
+    elsif caseRoll <= 0.5
+        rarity = 2
+    else
+        rarity = 1
+    end
+
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+
+    user_id = db.execute("SELECT id FROM user WHERE id = ?", decoded_token[0]['sub'])
+    item_id = db.execute("SELECT id FROM item WHERE case_id = #{caseid} AND rarity_class = #{rarity}")
+
+    db.execute("INSERT INTO u_item (user_id, item_id, condition_float) VALUES (?, ?, ?)", user_id[0]["id"], item_id[0]["id"], condition_float)
+
+    db.close
+
+    redirect('/')
 end
